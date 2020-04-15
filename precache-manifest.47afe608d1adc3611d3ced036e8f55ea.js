908573fb9c66c329453b99781a6fa26c66104668
brianbellini <brianbellini@me.com>
2020-04-15 15:10:56 -0600

    Updates

---
+++ precache-manifest.47afe608d1adc3611d3ced036e8f55ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67d872dbf743658bc162859f326c807d",
+    "url": "/brianbellini/project-4-assessment.git/index.html"
+  },
+  {
+    "revision": "d69b07ad6a9ede96b6c3",
+    "url": "/brianbellini/project-4-assessment.git/static/css/main.2ddb3bd6.chunk.css"
+  },
+  {
+    "revision": "bf9faed5984ec60f7781",
+    "url": "/brianbellini/project-4-assessment.git/static/js/2.36c0ca1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/brianbellini/project-4-assessment.git/static/js/2.36c0ca1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d69b07ad6a9ede96b6c3",
+    "url": "/brianbellini/project-4-assessment.git/static/js/main.60c2c164.chunk.js"
+  },
+  {
+    "revision": "05965cc5601718a3c9a5",
+    "url": "/brianbellini/project-4-assessment.git/static/js/runtime-main.8703f293.js"
+  }
+]);
